9a9caecc5abdd1871922c987dda5caa24ef9bd84
Bruna <brunabosco@hotmail.com>
2025-01-27 13:11:10 -0300

    01_Componente_Home_navbar_footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-red-600 text-white'>
+            
+                <div className="container flex justify-between text-lg">
+                    Farmácia Longa Vida
+
+                    <div className='flex gap-4'>
+                        Produto
+                        Categoria
+                        Cadastrar categoria
+                        Perfil
+                        Sair
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
